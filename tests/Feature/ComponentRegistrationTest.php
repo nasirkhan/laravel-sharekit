f1e142914de8cb37eeb0d66433b5448c326c653a
Nasir Khan Saikat <nasir8891@gmail.com>
2026-04-08 00:27:42 +0600

    test: Update ComponentRegistrationTest to use class aliases for components

--- tests/Feature/ComponentRegistrationTest.php
+++ tests/Feature/ComponentRegistrationTest.php
@@ -3,6 +3,9 @@
 namespace Nasirkhan\LaravelSharekit\Tests\Feature;
 
 use Nasirkhan\LaravelSharekit\Tests\TestCase;
+use Nasirkhan\LaravelSharekit\View\Components\Button;
+use Nasirkhan\LaravelSharekit\View\Components\Buttons;
+use Nasirkhan\LaravelSharekit\View\Components\Icon;
 use PHPUnit\Framework\Attributes\Test;
 
 class ComponentRegistrationTest extends TestCase
@@ -10,8 +13,8 @@ class ComponentRegistrationTest extends TestCase
     #[Test]
     public function it_registers_share_components(): void
     {
-        $this->assertTrue(class_exists(\Nasirkhan\LaravelSharekit\View\Components\Buttons::class));
-        $this->assertTrue(class_exists(\Nasirkhan\LaravelSharekit\View\Components\Button::class));
-        $this->assertTrue(class_exists(\Nasirkhan\LaravelSharekit\View\Components\Icon::class));
+        $this->assertTrue(class_exists(Buttons::class));
+        $this->assertTrue(class_exists(Button::class));
+        $this->assertTrue(class_exists(Icon::class));
     }
 }
